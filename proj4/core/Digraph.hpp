edc2f6034f8d3aabd7e9572b3f216409d6b7f1bf
JoowonSuh <sjw2023@naver.com>
2018-12-09 19:13:11 -0800

    Debugging isStronglyConnected

--- proj4/core/Digraph.hpp
+++ proj4/core/Digraph.hpp
@@ -444,7 +444,7 @@ bool Digraph<VertexInfo,EdgeInfo>::checkConnectivity(int from,std::vector<int> v
     std::vector<int> vertice = vertices();
     for(typename std::list<DigraphEdge<EdgeInfo>>::const_iterator it = info.at(from).edges.begin();it!=info.at(from).edges.end();it++)
     {
-        std::cout << "isStronglyConnected : "<< it->toVertex;
+        std::cout << "checkConnectivity : "<< it->toVertex;
         if(std::find(visited.begin(),visited.end(),it->toVertex)==visited.end())
             visited.push_back(it->toVertex);
         else if(visited.size() < vertice.size()-1)
